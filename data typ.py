8a54b9a6ab6ae39129c8fa29b761eaa27978d0d5
Ronak7399 <ronaknaik7399@gmail.com>
2024-07-06 10:50:03 +0530

    first commit

---
+++ data typ.py
@@ -0,0 +1,47 @@
+# numerical datatype
+a=12
+print(a)
+
+aa=45
+print (a)
+
+b=12.423       #floting point
+print(b)
+
+print(type(a))
+print(type(b))
+
+# datatype is type of data that we are going to do some action with it
+ 
+# bool datatype 
+
+c = True 
+d = False
+
+print (c)
+print (d)
+  
+print(type(c))
+print(type(d))
+
+# string data type # string is a group character
+
+name = 'Bascom'
+name2 = "Bascom"  #str
+print (name)
+print (name2)
+
+print(type(name))
+print(type(name2))
+
+#  "hello"
+value ='"Hello"'
+print(value)
+
+value2 = "'Hello'"
+print(value2)
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,12 @@
+# variables  are the nam wich stores the values
+a=12 
+A=13
+_a=14
+a2=123
+
+b=13
+
+print(a+b)
+
+print(10+20)
+
