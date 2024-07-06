a7e37293208430c741304883e59b72388f2bc682
Ronak7399 <ronaknaik7399@gmail.com>
2024-07-06 12:15:05 +0530

    user input,extra datatype

---
+++ extra datatype.py
@@ -0,0 +1,62 @@
+# number // string // boolean 
+# list
+
+# list => group oof datatype under single datatype name
+
+data = [12,34,15,16,17,18,24,25,26,30,40]
+
+print(data)
+print(type(data))
+
+values = [121212,12,34,"bascom",'jimit',True]
+
+# we can acces list values using index
+
+print(values) 
+print(type(values))
+
+print(values[4])
+values[2] = "maru bascom"
+
+print (values)
+  
+##### added values in list#####
+  
+mydata=[]    #empty list 
+  
+print(mydata)
+  
+mydata.append(12)
+mydata.append(12.34)
+mydata.append("ronak cod lern kar") 
+
+print (mydata)
+values.append("ronak cod lern kar")
+
+print(values)
+
+# values.clear() #empty the list
+
+#print(valuesd)
+
+print(len(values))  # find the size of the list
+
+values.reverse()
+
+print(values)
+
+numbers = [1,14,12,156,17,18,35,45,65,78]
+
+print(numbers)
+
+numbers.sort()
+numbers.reverse()
+
+print(numbers)
+
+numbers.remove(17)
+print(numbers)
+
+
+ 
+ 
---
+++ user_input.py
@@ -0,0 +1,9 @@
+# input function to take input from user
+
+a = input("plese enter first name:")
+b = input("please enter last name:")
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
