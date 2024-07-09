9618ad9b91be5b5fad18303f53e1e67be6cb4d19
Ronak7399 <ronaknaik7399@gmail.com>
2024-07-09 11:33:28 +0530

    operators.py

--- operators.py
+++ operators.py
@@ -24,3 +24,77 @@ b = 2
 print(a/b)
 print(a//b)
 
+# conditional operators + < , > <= >= == !=
+# if else statement
+# conditional statement give me boolean in return 
+
+h = 12
+j =13
+
+k = h<j
+
+print(k)
+print(type(k))
+
+# equality operator ==
+
+h1 = 12
+j1 = 13
+
+print(h1==j1)
+
+print(h1 != j1)
+
+# ** arithmatic operator
+
+print(15**3)
+ 
+# assignment operator
+# = += -= /= //= %=  **=
+
+d = 23
+d += 10
+
+print(d)
+
+d = 45
+d += 10
+
+print(d)
+
+d1 = 45
+d1 /= 10
+print(d1)
+
+d2 = 56
+d2 %= 10
+print(d2)
+
+# logical operator 
+# and or # if else
+# use between 2 conditional operator 
+
+j1 = 34
+j2 = 35
+
+print(j1<j2 and j1<j2)
+
+
+k1 = 23
+k2 = 45
+ 
+k3 =  k1<k2 or k1>k2
+
+print(k3)
+print(type(k3))
+name = str(k3)
+print(name)
+print(type(name))
+
+value = 45
+name1 = str(value)
+print(value)
+print(name1)
+print(type(name1))
+
+
