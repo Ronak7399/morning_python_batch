7b9b2c174b2fedcebd0f7f51a67dffa6bd24c1a6
Ronak7399 <ronaknaik7399@gmail.com>
2024-07-11 13:19:16 +0530

    new

--- extra datatype.py
+++ extra datatype.py
@@ -59,6 +59,141 @@ print(numbers)
 
 # list datatype deatail
 
+number = [12,34,354,34,4,2,4,True,"bascom",[1,2,3]]
+
+print(number[9])
+
+a = [1,2,3,4,5]
+
+print(a)
+print(a[2])
+a[2] = 6
+print(a)
+
+a.append(45)        
+a.append(56)
+print(a)
+
+a.insert(4,10)
+print(a)
+
+a.pop()
+a.pop()
+print(a)
+
+a.remove(4)
+print(a)
+
+a.extend([9,8,7])
+print(a)
+
+a.append([9,8,7])
+print(a)
+
+a.append('bascom')  # type: ignore
+print(a)
+
+list=['hello','hi','how are you','hola','hi']
+
+name = "bascom bridge"
+   # string is immutable
+name = name.capitalize()
+print(name)
+
+# a.clear()
+# print()
+
+names = [] # empty string
+
+b = a.copy()
+
+print(b)
+print(list.count('hi'))
+print(list.index('hola'))
+print(list)
+list.reverse()
+print(list)
+list.sort()
+print(list)
+
+###########################################
+
+# tuple
+
+adress = (1,2,3,4,5,6,7,8,9) # immutable
+print(adress)
+print(type(adress))
+
+print(len(adress))
+
+###########################################
+
+# num = 12
+
+# if num in range(1,2)
+#       print("He")
+
+############################################
+
+# dictionary
+# key - value
+
+user = {
+    "name":"ronak",
+    "age" : 24,
+    "adress":"gandhinagar",
+    "name": "rohan",
+    "name": "majanubhai",
+    True:[1,2,3],
+    "Marks": (12,13,14)
+ }
+
+print(user)
+
+g = ["name","adress","age"]
+k = 10
+
+data = {} 
+
+print(data.fromkeys(g,k))  # many to one relation
+
+print(user.get("name"))
+
+print(user.items())
+
+user.pop("age")
+
+print(user)
+
+user.popitem()
+print(user)
+
+user.setdefault("pata","valsad")
+
+print(user)
+
+user.update({"name":"udey bhai"})
+print(user)
+
+print(user.values())
+
+# swap number without using third variable
+
+a1 = 12
+b1 = 13
+
+print(a1)
+print(b1)
+
+a1= a1+b1 #25
+b1= a1-b1 #25-13 # 12
+a1= a1-b1 #25-12 #13
+
+
+
+
+
+
 
  
  
