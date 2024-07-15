ba6bfdaba71a8c42de64bc43681011e441867b75
Ronak7399 <ronaknaik7399@gmail.com>
2024-07-15 15:57:16 +0530

    program4

---
+++ program4.py
@@ -0,0 +1,39 @@
+# question 1 
+
+for n in range(1,10):
+    print(n)
+    
+# question 2 
+
+for n in range (1,20,2):
+    print(n)
+    
+# question 3
+
+list = [1,2,3,4,5]
+
+for n in list:
+    print(n)
+    
+# question 4 
+
+list = [1,2,3,4,5]
+sum=0
+for val in list:
+    sum=sum+val
+    
+    print(sum)
+    
+# question 5
+
+list = ["hello"]
+
+for n in list:
+    print(n)
+    
+
+
+    
+    
+
+
