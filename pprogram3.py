7094511f45889c7c1de4488fa1c64493b84eb3e2
Ronak7399 <ronaknaik7399@gmail.com>
2024-07-15 14:55:27 +0530

    program 3

---
+++ pprogram3.py
@@ -0,0 +1,63 @@
+# question 1 
+
+for  a in range(1,101):
+    print(a)
+    
+# question 2
+
+n=1
+
+while n<=9:
+    print(n)
+    n = n+1
+print(n) 
+
+# question 3 
+
+for x in range(1,20):
+    if x%2!=0:
+        print(x)
+
+
+# question 4 
+
+n = 2
+
+while n<=18:
+    print(n)
+    n = n+2
+    
+print(n)
+      
+# question 5
+
+for contdown in range(10,0,-1):
+
+    print(contdown)
+    
+# question 6 
+
+x = 10 
+
+while x>=1:
+    print(x)
+    x = x-1
+ 
+# question 7
+
+num = 16
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
+   
+
+      
+    
+
